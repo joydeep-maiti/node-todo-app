bd42b01dfa01a09f8bae1f6687738519b58b03dd
Joydeep Maiti <joydeep127@gmail.com>
2018-02-22 21:05:55 +0530

    GET / routes

--- server/server.js
+++ server/server.js
@@ -33,4 +33,22 @@ app.post('/users', (req, res) => {
         res.status(400).send(e);
     });
 });
+
+app.get('/todos', (req, res)=> {
+    Todo.find().then((todos)=> {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+    
+});
+
+app.get('/users', (req, res) => {
+    User.find().then((users) => {
+        res.send({users});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
 app.listen(3000);
